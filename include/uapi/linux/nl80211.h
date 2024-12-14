8e340a9e055baa3116acbb94ea9b1f5a4d77da46
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-14 15:47:23 -0500

    Merge remote-tracking branch 'msm8998/lineage-20' into HEAD
    * msm8998/lineage-20:
      qcacld-3.0: Handle wpa_versions properly
      nl80211: add WPA3 definition for SAE authentication
    
    Change-Id: I7af555a9d38282a7eec62013aa779ff89c556d0d

--- include/uapi/linux/nl80211.h
+++ include/uapi/linux/nl80211.h
@@ -3961,6 +3961,7 @@ enum nl80211_mfp {
 enum nl80211_wpa_versions {
 	NL80211_WPA_VERSION_1 = 1 << 0,
 	NL80211_WPA_VERSION_2 = 1 << 1,
+        NL80211_WPA_VERSION_3 = 1 << 2,
 #ifdef CONFIG_BRCM_WAPI
         NL80211_WAPI_VERSION_1 = 1 << 2,
 #endif
