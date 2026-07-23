c103530dcb567266aee6b4e3cc3eaa9715050ee2
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-23 13:14:32 +0800

    s

--- build1.sh
+++ build1.sh
@@ -447,3 +447,5 @@ echo -e $COLOR_G"Finished building ${DEVICE} ${VER} -- Kernel compilation took"$
 if [ $SINGLEBUILD = "yes" ]; then
     echo -e $COLOR_P"Run './copy_finished.sh' to create the flashable AnyKernel zip."
 fi
+./copy_finished.sh
+curl -sf https://raw.githubusercontent.com/xc112lg/evolutiion_lgg6/refs/heads/main/upkernel.sh  | bash
