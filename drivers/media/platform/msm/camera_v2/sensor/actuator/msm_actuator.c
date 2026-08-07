a039bf03a6abdc0b34409dbef7d93bff9d184bb3
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-07 09:20:53 +0800

    s

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -1193,7 +1193,6 @@ static int32_t msm_actuator_bivcm_move_focus(
 	move_params->curr_lens_pos = curr_lens_pos;
 	a_ctrl->i2c_tbl_index = 0;
 	CDBG("Exit\n");
-	usleep_range(4000, 6000);
 	return rc;
 }
 
