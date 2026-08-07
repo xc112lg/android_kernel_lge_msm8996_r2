ff161d81040ab8dc4de1f71074deb15beaa00f59
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-07 09:26:04 +0800

    d

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -717,7 +717,7 @@ static int32_t msm_actuator_move_focus(
 	}
 	a_ctrl->i2c_tbl_index = 0;
 	CDBG("Exit\n");
-	usleep_range(4000, 6000);
+
 	return rc;
 }
 
@@ -1072,6 +1072,9 @@ static int32_t msm_actuator_claf_move_focus(
 	}
 	CDBG("Exit\n");
 
+	/* mitigate mm-qcamera-daemon PDAF race on direction change */
+	usleep_range(4000, 6000);
+
 	return rc;
 }
 #endif
@@ -1193,6 +1196,7 @@ static int32_t msm_actuator_bivcm_move_focus(
 	move_params->curr_lens_pos = curr_lens_pos;
 	a_ctrl->i2c_tbl_index = 0;
 	CDBG("Exit\n");
+
 	return rc;
 }
 
